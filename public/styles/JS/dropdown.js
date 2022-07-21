27e9946c327dea5696b60b9bc24b8363075ec03c
LaleskaSalles <laleskasalomao@gmail.com>
2022-07-21 14:28:21 -0300

    dropdown div funcionando

--- public/styles/JS/dropdown.js
+++ public/styles/JS/dropdown.js
@@ -1,17 +1,9 @@
-window.onload = function (){
-    let btnDropDown = document.querySelector('.btnDropDownForm');
-    let divDropDown = document.querySelector('.dropDownForm');
-    let setaDropDown = document.querySelector('.setaDropDown');
-
-    btnDropDown.addEventListener('click', () =>{
-        if(divDropDown.style.display === 'none'){
-            divDropDown.style.display = 'block';
-            setaDropDown.style.transform = 'translateY(10%) rotate(140deg)';
-
-        }else{
-            divDropDown.style.display = 'none';
-            setaDropDown.style.transform = 'translateY(10%) rotate(-45deg)';
+$(document).ready(function (){
+    $(".btnDropDownForm").click(function (){
+        if($(".dropDownForm").is(":visible")){
+            $(this).siblings(".dropDownForm").hide("slow");
+        } if($(".dropDownForm").is(":hidden")){
+            $(this).siblings(".dropDownForm").show("slow");
         }
-    })
-};
-
+    });
+});
