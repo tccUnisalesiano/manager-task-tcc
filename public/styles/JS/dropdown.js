110d372541c4aa587f783cb267cc32b809b63203
LaleskaSalles <laleskasalomao@gmail.com>
2022-07-21 16:02:48 -0300

    dropdown sem animação

--- public/styles/JS/dropdown.js
+++ public/styles/JS/dropdown.js
@@ -1,9 +1,5 @@
 $(document).ready(function (){
     $(".btnDropDownForm").click(function (){
-        if($(".dropDownForm").is(":visible")){
-            $(this).siblings(".dropDownForm").hide("slow");
-        } if($(".dropDownForm").is(":hidden")){
-            $(this).siblings(".dropDownForm").show("slow");
-        }
+        $(this).siblings(".dropDownForm").toggle("slow");
     });
 });
