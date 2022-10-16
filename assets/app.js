3dfda904269f16f31f6713abb733f5cc6e9e350c
Guilherme Correia <61031607+guilhermebcorreia@users.noreply.github.com>
2022-10-16 18:58:21 -0300

    fix dataTables

--- assets/app.js
+++ assets/app.js
@@ -11,7 +11,13 @@
 import '../public/styles/css/navbar.css';
 import '../public/styles/css/ajusteBootstrap.css';
 import '../public/styles/JS/calendario.js';
-
+import 'datatables.net-bs4';
+const $ = require('jquery');
+import 'datatables.net-bs4';
+import 'datatables.net/js/jquery.dataTables';
+import 'datatables.net-bs4/js/dataTables.bootstrap4';
+import 'datatables.net-bs4/css/dataTables.bootstrap4.css';
+global.$ = global.jQuery = $;
 
 
 // start the Stimulus application
