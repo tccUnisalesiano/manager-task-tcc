d7b1199e7e615207f82c3d57803e94f19765bcd1
Guilherme Correia <61031607+guilhermebcorreia@users.noreply.github.com>
2022-10-30 18:41:29 -0300

    fix sql

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -38,7 +38,29 @@ class DashboardController extends AbstractController
     public function projetos(EntityManagerInterface $em, Request $request, ManagerRegistry $registry, ProjetoRepository $projeto): JsonResponse|NotFoundHttpException
     {
 
-        $response = $projeto->findAllProdutos();
+        $response = $projeto->findAllProjetos();
+
+        if(!$response) return new NotFoundHttpException();
+
+        return $this->json([
+            'success' => true,
+            'response' => count($response)
+        ]);
+    }
+
+    /**
+     * @Route("/dashboard/projetosAberto", methods={"POST"})
+     * @param EntityManagerInterface $em
+     * @param Request $request
+     * @param ManagerRegistry $registry
+     * @param ProjetoRepository $projeto
+     * @return JsonResponse|NotFoundHttpException
+     * @throws Exception
+     */
+    public function projetosAberto(EntityManagerInterface $em, Request $request, ManagerRegistry $registry, ProjetoRepository $projeto): JsonResponse|NotFoundHttpException
+    {
+
+        $response = $projeto->findAllProjetosAberto();
 
         if(!$response) return new NotFoundHttpException();
 
